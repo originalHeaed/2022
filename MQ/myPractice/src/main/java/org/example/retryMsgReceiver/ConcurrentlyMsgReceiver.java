dc353d4d2903dce6f6430f41ae9ff4a6fb487c98
wanggh31690 <1879519397@qq.com>
2023-02-10 16:22:05 +0800

    mq - 消费者消费失败重试

---
+++ MQ/myPractice/src/main/java/org/example/retryMsgReceiver/ConcurrentlyMsgReceiver.java
@@ -0,0 +1,49 @@
+package org.example.retryMsgReceiver;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * 无序消息消费失败重试
+ */
+public class ConcurrentlyMsgReceiver {
+    /**
+     * namesrv 的 ip:port
+     */
+    public static String NAME_SRV = "112.74.97.91:9876";
+
+    /**
+     * 消息 topic
+     */
+    public static String TOPIC = "retryConcurrentlyTp";
+
+    /**
+     * 消费者 group
+     */
+    public static String GP = "retryConcurrentlyGP";
+
+    public static void main(String[] args) throws MQClientException {
+        /* 1. 定义消费者 */
+        DefaultMQPushConsumer defaultMQPushConsumer = new DefaultMQPushConsumer(GP);
+        /* 2. 进行配置 */
+        defaultMQPushConsumer.setNamesrvAddr(NAME_SRV);
+        /* todo 对于无序消息，不能直接直接消费超时时间，其消息消费超时时间有自己的规则，可以指定其最大消费重试次数（默认 16）  */
+        defaultMQPushConsumer.setMaxReconsumeTimes(32);
+        defaultMQPushConsumer.subscribe(TOPIC, "*");
+        defaultMQPushConsumer.registerMessageListener((List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) -> {
+            /* 如果结果返回 ConsumeConcurrentlyStatus.RECONSUME_LATER、null、抛出异常 该消息后续将会进行
+             * 失败重试，需要进行重试的失败消息将会放在延迟队列中，等待指定时间间隔过后，在放回正常队列中进行消费 */
+            return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+        });
+        /* 3. 启动消费者 */
+        defaultMQPushConsumer.start();
+    }
+
+}
---
+++ MQ/myPractice/src/main/java/org/example/retryMsgReceiver/OrderlyMsgRetryReceiver.java
@@ -0,0 +1,54 @@
+package org.example.retryMsgReceiver;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * 顺序消息重复消费
+ */
+public class OrderlyMsgRetryReceiver {
+
+    /**
+     * namesrv 的 ip:port
+     */
+    public static String NAME_SRV = "112.74.97.91:9876";
+
+    /**
+     * 消息 topic
+     */
+    public static String TOPIC = "retryOrderlyTp";
+
+    /**
+     * 消费者 group
+     */
+    public static String GP = "retryOrderlyGP";
+
+    public static void main(String[] args) throws MQClientException {
+        /* 1. 创建消费者 */
+        DefaultMQPushConsumer defaultMQPushConsumer = new DefaultMQPushConsumer(GP);
+        /* 2. 进行配置 */
+        defaultMQPushConsumer.setNamesrvAddr(NAME_SRV); // namesrv ip:port
+        defaultMQPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET); // 从哪里开始消费
+        defaultMQPushConsumer.subscribe(TOPIC, "*");
+        // todo 设置顺序消息的消费超时时间，取值范围：[10, 30000]，默认值：1000，单位 ms
+        defaultMQPushConsumer.setSuspendCurrentQueueTimeMillis(3000);
+        // todo 对于顺序消息来说，该字段可以设置顺序消息消费失败重试的最大值，默认顺序消费的最大值 java 中 int 的最大值
+        defaultMQPushConsumer.setMaxReconsumeTimes(1000);
+        defaultMQPushConsumer.registerMessageListener((List<MessageExt> list, ConsumeOrderlyContext consumeOrderlyContext) -> {
+            /* 1. 使用顺序消费方式拉取消息，然后按照指定的方式进行排序，存放在 list 中 */
+            /* 2. 如果结果返回 ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT、null、抛出异常 该消息后续将会进行
+            * 失败重试，但是需要注意，在顺序消息消费过程中，如果某条消息消费失败，则该消费者组无法消费后续其他的消息 */
+            return ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT;
+        });
+        /* 3. 启动消费者 */
+        defaultMQPushConsumer.start();
+    }
+}
