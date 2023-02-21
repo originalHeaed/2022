578fe31d18f24b603ec5317a14b714d4cd9ea738
wanggh31690 <1879519397@qq.com>
2023-02-21 21:35:48 +0800

    测试线程中断

--- MQ/myPractice/src/main/java/org/example/Test.java
+++ MQ/myPractice/src/main/java/org/example/Test.java
@@ -1,36 +1,17 @@
-import org.junit.Test;
+package org.example;
 
-public class MyTest {
-
-    @Test
-    public void test1() {
-        int i = 0;
-        while (true) {
-            label1: {
-                if (i != 5) {
-                    System.out.println("当前 i 值为：" + i);
-                    break label1;
-                } else {
-                    System.out.println("i 值为：" + i + " 循环结束");
-                    return;
-                }
-            }
-            i++;
-        }
-    }
-
-    @Test
-    public void test2() throws InterruptedException {
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
         /* 启动子线程 */
         Thread thread = new Thread(new TestClass());
         thread.start();
         /* 使用父线程进行中断测试 */
-        Thread.sleep(10000);
+        Thread.sleep(5000);
         System.out.println(Thread.currentThread().toString() + "正在尝试中断子线程：" + thread.toString());
         thread.interrupt(); // 中断子线程
     }
 
-    class TestClass implements Runnable {
+    static class TestClass implements Runnable {
         @Override
         public void run() {
             while (true) {
@@ -39,8 +20,10 @@ public class MyTest {
                     System.out.println("程序继续进行中...");
                 } catch (Exception e) {
                     System.out.println("当前线程：" + Thread.currentThread().toString() + " 被中断");
+                    break;
                 }
             }
         }
     }
+
 }
